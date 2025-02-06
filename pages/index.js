db4c77a8a07ecfa023c6aacb879ca814a6975a9f
Gabriel H. R. Caldeira <79008744+GabrielHenrique0@users.noreply.github.com>
2025-02-06 03:12:25 +0000

    finalizando 1º desafio

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,16 @@ function Home() {
     const [nome, setNome] = useState("");
 
     return (
-        <div style={{ textAlign: "center", marginTop: "50px", display: "flex", flexDirection: "column" }}>
+        <div style={{ textAlign: "center", marginTop: "50px", display: "flex", flexDirection: "column", alignItems: "center" }}>
             <h1>Oi mãe, digita seu nome aí kkk</h1>
             <input 
                 type="text" 
                 placeholder="Digite aqui..." 
                 value={nome} 
                 onChange={(e) => setNome(e.target.value)} 
-                style={{ padding: "10px", fontSize: "16px", marginBottom: "10px" }}
+                style={{ padding: "10px", fontSize: "16px", marginBottom: "10px", width: "250px" }}
             />
-            <strong>Olá, {nome}! =)</strong>
+            {nome.trim() && <p><strong>Olá, {nome}! =)</strong></p>}
         </div>
     );
 }
