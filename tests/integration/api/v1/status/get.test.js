8485fb6651c88e26e36de4feab41676831660db6
Gabriel H. R. Caldeira <79008744+GabrielHenrique0@users.noreply.github.com>
2025-03-23 04:32:41 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,29 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  // expect(responseBody.postgres_version).toBeDefined();
+  // expect(responseBody.max_connections).toBeDefined();
+  // expect(responseBody.current_connections).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
+  // expect(typeof responseBody.postgres_version).toBe("string");
+  // expect(responseBody.postgres_version).toMatch(/PostgreSQL\s+\d+(\.\d+)?/);
+
+  // expect(typeof responseBody.max_connections).toBe("number");
+  // expect(Number.isInteger(responseBody.max_connections)).toBe(true);
+
+  // expect(typeof responseBody.current_connections).toBe("number");
+  // expect(Number.isInteger(responseBody.current_connections)).toBe(true);
+
+  // expect(responseBody.current_connections).toBeLessThanOrEqual(
+  //   responseBody.max_connections,
+  // );
 });
