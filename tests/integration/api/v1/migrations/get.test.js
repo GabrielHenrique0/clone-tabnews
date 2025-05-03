01005d3be222e273829c85e01d6422fe6b3327b2
Gabriel H. R. Caldeira <79008744+GabrielHenrique0@users.noreply.github.com>
2025-05-03 17:42:40 +0000

    remove console.log

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,7 +8,6 @@ async function cleanDatabase() {
 }
 
 test("GET to /api/v1/migrations should return 200", async () => {
-  console.log("Ambiente atuaaaal:", process.env.NODE_ENV);
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
