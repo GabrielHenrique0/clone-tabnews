b5cdeec2afc0406b04db41507fcdc468619f1bf8
Gabriel H. R. Caldeira <79008744+GabrielHenrique0@users.noreply.github.com>
2025-09-19 03:15:57 +0000

    adds new rules and ignores to `eslint.config.js` and fix parsing error

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
 const js = require("@eslint/js");
 
 module.exports = [
+  {
+    ignores: [
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.turbo/**",
+    ],
+  },
   js.configs.recommended,
   {
     rules: {
       "no-unused-vars": "warn",
+      "no-console": "warn",
+      "no-debugger": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      "no-undef": "off", // desativa os erros de variáveis indefinidas
     },
   },
 ];
