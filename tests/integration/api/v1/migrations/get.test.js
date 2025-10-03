66687fa9fc8d3bbef7d4945087b73064a130c746
Gabriel H. R. Caldeira <79008744+GabrielHenrique0@users.noreply.github.com>
2025-10-03 03:26:21 +0000

    fix `eslint` linting

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,8 +7,6 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-async function cleanDatabase() {}
-
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
@@ -18,9 +16,3 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
 });
-
-// test("Check if the development environment .env file has been loaded.", () => {
-//   dotenv.config({ path: ".env.development" });
-//   console.log("Variável de ambiente POSTGRES_USER:", process.env.POSTGRES_USER);
-//   expect(process.env.POSTGRES_USER).toBeDefined();
-// });
